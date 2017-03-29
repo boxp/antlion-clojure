c6206b7080b2bea87f1745c82f02cdcdec5e70b4
boxp <tiyotiyouda@gmail.com>
2017-03-29 11:23:59 +0900

    ReplicationController -> Deployment

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ set -e
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/antlion-clojure:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f k8s/contrib/pets/redis/redis.yaml
-kubectl apply -f k8s/controller.yml
+kubectl apply -f k8s/deployment.yml
