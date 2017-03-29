41f385c97b41e1abb3618b9e2b169f1f549f89e6
boxp <tiyotiyouda@gmail.com>
2017-03-29 19:04:12 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ for f in k8s/*.yml
 do
 	envsubst < $f > "generated-$(basename $f)"
 done
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/antlion-clojure:latest
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/antlion-clojure:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f k8s/contrib/pets/redis/redis.yaml
 kubectl apply -f generated-deployment.yml
