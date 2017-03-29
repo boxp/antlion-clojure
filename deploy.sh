30c73a466cb8a6772fade296ad6916ac1f85819e
boxp <tiyotiyouda@gmail.com>
2017-03-29 18:46:26 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 # Exit on any error
 set -e
 
+for f in k8s/*.yml
+do
+	envsubst < $f > "generated/$(basename $f)"
+done
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/antlion-clojure:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f k8s/contrib/pets/redis/redis.yaml
-kubectl apply -f k8s/deployment.yml
+kubectl apply -f generated/deployment.yml
