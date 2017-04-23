7a631279cf43bc47bbb207149c701eb7114907a8
boxp <tiyotiyouda@gmail.com>
2017-04-23 14:59:42 +0900

    WIP

--- src/antlion_clojure/github.clj
+++ src/antlion_clojure/github.clj
@@ -1,2 +1,16 @@
 (ns antlion-clojure.github
-  (:require [tentacles.users :as users]))
+  (:require [tentacles.core :as core]
+            [tentacles.users :as users]
+            [tentacles.repos :as repos]
+            [environ.core :refer [env]]
+            [com.stuartsierra.component :as component]))
+
+(defn base-option
+  []
+  {:oauth-token (env :antlion-clojure-github-oauth-token)
+   :accept "application/vnd.github.black-cat-preview+json"})
+
+(defn reviewers
+  [url]
+  (core/api-call :get "%s/requested_reviewers"
+                 (base-option)))
