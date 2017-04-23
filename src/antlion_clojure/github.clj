453249f1ff004b251043dc2df265d5debca27065
boxp <tiyotiyouda@gmail.com>
2017-04-23 17:25:54 +0900

    Add support github automate review assign

--- src/antlion_clojure/github.clj
+++ src/antlion_clojure/github.clj
@@ -2,8 +2,23 @@
   (:require [tentacles.core :as core]
             [tentacles.users :as users]
             [tentacles.repos :as repos]
+            [tentacles.pulls :as pulls]
             [environ.core :refer [env]]
             [com.stuartsierra.component :as component]))
+
+(defrecord PullRequest
+  [owner repo id])
+
+(defn url->pull-request
+  [url]
+  (some->> url
+           (drop 1)
+           (drop-last)
+           (apply str)
+           (re-seq #"https\://github\.com/(.*)/(.*)/pull/([0-9]*)$")
+           first
+           (drop 1)
+           (apply ->PullRequest)))
 
 (defn base-option
   []
@@ -11,6 +26,14 @@
    :accept "application/vnd.github.black-cat-preview+json"})
 
 (defn reviewers
-  [url]
-  (core/api-call :get "%s/requested_reviewers"
-                 (base-option)))
+  [pr]
+  (->> (pulls/specific-pull (:owner pr) (:repo pr) (:id pr) (base-option))
+       :requested_reviewers))
+
+(defn add-reviewer
+  [url reviewer]
+  (let [pr (url->pull-request url)]
+    (core/api-call :post "repos/%s/%s/pulls/%s/requested_reviewers"
+                   [(:owner pr) (:repo pr) (:id pr)]
+                   (merge (base-option)
+                          {:reviewers [reviewer]}))))
